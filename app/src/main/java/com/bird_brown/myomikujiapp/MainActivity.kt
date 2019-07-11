1c4edb31e8914bdedcef01d30e6f4e4e96ba2b37
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-07-11 21:49:49 +0900

    Click listener commit

--- app/src/main/java/com/bird_brown/myomikujiapp/MainActivity.kt
+++ app/src/main/java/com/bird_brown/myomikujiapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.bird_brown.myomikujiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.R
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        setContentView(R.layout.content_layout_id)
+
+        getOmikujiButton.setOnClickListener {
+            // Code here executes on main thread after user presses button
+        }
     }
 }
